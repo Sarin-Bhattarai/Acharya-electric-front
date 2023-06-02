4921f201e227497875ef32c7f6f5e71aedbdb327
sarin punk <sarin.bhattarai@icp.edu.np>
2023-06-02 12:50:00 +0545

    feat(changes on about)

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,8 +1,8 @@
 import "./about.css";
 import React from "react";
 import { Button } from "antd";
-import C1 from "../../resources/images/c1.jpg";
-import C2 from "../../resources/images/c2.jpg";
+import C1 from "../../resources/images/mee.jpg";
+import Dokan from "../../resources/images/dokan.jpg";
 import { useNavigate } from "react-router-dom";
 
 const About = () => {
@@ -38,7 +38,7 @@ const About = () => {
             </Button>
           </div>
           <div className="aboutImg">
-            <img src={C2} alt="c1" />
+            <img src={Dokan} alt="c1" />
           </div>
         </div>
 
@@ -48,23 +48,70 @@ const About = () => {
           </div>
           <div className="heading">
             <h5>HIGH QUALITY SERVICE</h5>
-            <h1>WHY CHOOSE OUR</h1>
-            <h1>COMPANY</h1>
+            <h1>WHY CHOOSE US</h1>
             <p
               style={{
                 marginBottom: "20px",
               }}
             >
-              We believe in building strong relationships with our clients and
-              take the time to understand their unique needs. We communicate
-              clearly and effectively throughout the entire process, ensuring
-              our clients are fully informed and happy with our work. Safety and
-              efficiency are also paramount to us. We use the latest technology
-              and tools to complete our work quickly and safely, minimizing
-              downtime. Overall, I am proud of the work that our electrical
-              company does. We are committed to providing exceptional electrical
-              services that exceed our clients' expectations. Thank you for
-              choosing us as your electrical service provider.
+              At our electric shop, we believe in the power of building strong
+              and lasting relationships with our valued clients. We prioritize
+              understanding their unique needs, going the extra mile to ensure
+              complete satisfaction. Our dedicated team takes the time to
+              listen, communicate clearly, and provide personalized solutions
+              tailored to each client. Throughout the entire process, we
+              maintain transparent and effective communication, keeping our
+              clients informed and involved, so they have full confidence in our
+              work.
+            </p>
+            <p
+              style={{
+                marginBottom: "20px",
+              }}
+            >
+              Safety and efficiency are fundamental principles that guide our
+              operations. We recognize the importance of a safe working
+              environment for both our team members and our clients. That's why
+              we adhere to the highest safety standards and invest in ongoing
+              training for our staff. We are equipped with the latest technology
+              and tools, enabling us to complete projects quickly and
+              efficiently, minimizing any potential downtime and disruption.
+            </p>
+            <p
+              style={{
+                marginBottom: "20px",
+              }}
+            >
+              Our team takes immense pride in the work we do. We approach each
+              project with unwavering dedication, attention to detail, and a
+              commitment to excellence. From small repairs to large-scale
+              installations, we deliver exceptional results that exceed our
+              clients' expectations. We understand that electrical services play
+              a critical role in our clients' lives, and we strive to make a
+              positive impact by providing reliable and innovative solutions.
+            </p>
+            <p
+              style={{
+                marginBottom: "20px",
+              }}
+            >
+              By choosing us as your electrical service provider, you are
+              placing your trust in a company that values your satisfaction
+              above all else. We are grateful for the opportunity to serve you
+              and work tirelessly to earn and maintain your trust. Our goal is
+              not just to meet your expectations but to surpass them, delivering
+              outstanding service that leaves a lasting impression.
+            </p>
+            <p
+              style={{
+                marginBottom: "20px",
+              }}
+            >
+              Thank you for considering us for your electrical needs. We look
+              forward to the opportunity to serve you and demonstrate the high
+              level of professionalism and expertise that sets us apart.
+              Together, let's create a strong and enduring partnership that will
+              keep your electrical systems functioning at their best.
             </p>
           </div>
         </div>
